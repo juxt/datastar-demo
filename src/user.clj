d2a228ab3eeaf1e5fbddfb55f510984eb213abb2
Malcolm Sparks <mal@juxt.pro>
2025-07-01 14:40:01 +0100

    Create a D* SSEGenerator type to send events with Helidon

--- src/user.clj
+++ src/user.clj
@@ -1,12 +1,31 @@
 (ns user
   (:require
    clojure.pprint
-   [s-exp.hirundo :as hirundo])
+   [clojure.string :as str]
+   [s-exp.hirundo :as hirundo]
+   [starfederation.datastar.clojure.protocols :as p]
+   [starfederation.datastar.clojure.api :as d*])
   (:import
    (io.helidon.http.sse SseEvent)
    (io.helidon.webserver.sse SseSink)))
 
-(defn handler [{:keys [:s-exp.hirundo.http.request/server-response request-method] :as req}]
+(deftype HelidonSseGenerator [sse-sink]
+  p/SSEGenerator
+  (send-event! [_ event-type data-lines opts]
+    (.emit sse-sink
+           (.. (SseEvent/builder)
+               (name event-type)
+               (data (str/join " " data-lines))
+               build)))
+  (get-lock [_] nil)
+  (close-sse! [_] (.close sse-sink))
+  (sse-gen? [_] true))
+
+(defn sse-generator [{:keys [:s-exp.hirundo.http.request/server-response] :as req}]
+  (let [sse-sink (.sink server-response SseSink/TYPE)]
+    (->HelidonSseGenerator sse-sink)))
+
+(defn handler [{:keys [request-method] :as req}]
   (case request-method
     :get
     {:status 200
@@ -14,11 +33,11 @@
      :body (slurp "index.html")}
 
     :post
-    (let [sse-sink (.sink server-response SseSink/TYPE)]
-      (doseq [i (range 10)]
-        (.emit sse-sink (SseEvent/create "Hello!"))
-        (Thread/sleep 500))
-      (.close sse-sink))))
+    (let [sse-gen (sse-generator req)]
+      (doseq [i (reverse (range 10))]
+        (d*/merge-fragments! sse-gen [(format "<div id='foo'>Starting in %d seconds !!!</div>" i)])
+        (Thread/sleep 1000))
+      (p/close-sse! sse-gen))))
 
 (defonce state (atom {}))
 
